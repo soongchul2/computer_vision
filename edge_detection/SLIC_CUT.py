0582902fd7a635d591788b741c19ccbee62c0773
soongchul2 <135080685+soongchul2@users.noreply.github.com>
2024-04-04 12:14:46 +0900

    Update SLIC_CUT.py

--- edge_detection/SLIC_CUT.py
+++ edge_detection/SLIC_CUT.py
@@ -9,8 +9,8 @@ start = time.time()
 
 slic = skimage.segmentation.slic(img, compactness = 20, n_segments = 600, start_label = 1)
 
-g = skimage.future.graph.rag_mean_color(img, slic, mode = 'similarity')
-ncut = skimage.future.graph.cut_normalized(slic, g)
+g = skimage.graph.rag_mean_color(img, slic, mode = 'similarity')
+ncut = skimage.graph.cut_normalized(slic, g)
 print(img.shape, 'image를 분할하는 데', time.time()-start, '초 소요')
 
 marking = skimage.segmentation.mark_boundaries(img,ncut)
